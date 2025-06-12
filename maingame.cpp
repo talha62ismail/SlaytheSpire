9fc54ed808661212960ddcdfd97440e4ce518ea4
Talha <User@LAPTOP-H70FCJ61>
2025-06-12 16:06:12 +0500

    sorting start window into a function

--- maingame.cpp
+++ maingame.cpp
@@ -3,29 +3,20 @@
 #include "raylib.h"
 using namespace std;
 
+void startwindow(Texture2D background);
+
 int main() {
 
 	InitWindow(1280, 720, "SLAY THE SPIRE");
 
     Texture2D background = LoadTexture("assets/mainbackground.png");
-    Rectangle start_button = { 540,335,200,50 };
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {  
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawTexture(background, 0, 0, WHITE);
-        DrawRectangleRec(start_button, BLACK);
-        DrawRectangleLines(545, 340, 190, 40, WHITE);
-        DrawText("START", start_button.x + 60, start_button.y + 15, 20, WHITE);
-
-        if (CheckCollisionPointRec(GetMousePosition(), start_button)) {
-            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-            {
-                cout << "GAME STARTED!";
-            }
-        }
+       
+        startwindow(background);
 
         EndDrawing();
     }
@@ -33,4 +24,21 @@ int main() {
     CloseWindow();
 
 	return 0;
+}
+
+void startwindow(Texture2D background) {
+    Rectangle start_button = { 540,335,200,50 };
+
+    ClearBackground(RAYWHITE);
+    DrawTexture(background, 0, 0, WHITE);
+    DrawRectangleRec(start_button, BLACK);
+    DrawRectangleLines(545, 340, 190, 40, WHITE);
+    DrawText("START", start_button.x + 60, start_button.y + 15, 20, WHITE);
+
+    if (CheckCollisionPointRec(GetMousePosition(), start_button)) {
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            cout << "GAME STARTED!";
+        }
+    }
 }
