e0f70b28807092b48905e3857ec89347710c42af
Talha <User@LAPTOP-H70FCJ61>
2025-06-12 15:36:04 +0500

    creating start window

--- maingame.cpp
+++ maingame.cpp
@@ -5,9 +5,10 @@ using namespace std;
 
 int main() {
 
-	InitWindow(1536, 1024, "SLAY THE SPIRE");
+	InitWindow(1280, 720, "SLAY THE SPIRE");
 
-    Texture2D background = LoadTexture("assets/enemy_level_background.png");
+    Texture2D background = LoadTexture("assets/mainbackground.png");
+    Rectangle start_button = { 540,335,200,50 };
 
     SetTargetFPS(60);
 
@@ -15,6 +16,17 @@ int main() {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawTexture(background, 0, 0, WHITE);
+        DrawRectangleRec(start_button, BLACK);
+        DrawRectangleLines(545, 340, 190, 40, WHITE);
+        DrawText("START", start_button.x + 60, start_button.y + 15, 20, WHITE);
+
+        if (CheckCollisionPointRec(GetMousePosition(), start_button)) {
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            {
+                cout << "GAME STARTED!";
+            }
+        }
+
         EndDrawing();
     }
 
