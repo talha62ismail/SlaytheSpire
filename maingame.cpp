dda30bf6018f2cfdac20f9837c3ced63923e8b9b
Talha <User@LAPTOP-H70FCJ61>
2025-06-12 13:53:40 +0500

    adding background

--- maingame.cpp
+++ maingame.cpp
@@ -5,12 +5,16 @@ using namespace std;
 
 int main() {
 
-	InitWindow(1000, 700, "SLAY THE SPIRE");
+	InitWindow(1536, 1024, "SLAY THE SPIRE");
+
+    Texture2D background = LoadTexture("assets/enemy_level_background.png");
+
+    SetTargetFPS(60);
 
     while (!WindowShouldClose()) {  
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("WELCOME TO SLAY THE SPIRE!", 150, 280, 20, DARKGRAY);
+        DrawTexture(background, 0, 0, WHITE);
         EndDrawing();
     }
 
